9dca2a5841790b1f4e6d7128d257f94dbfeec795
sshah7 <45990517+sshah7@users.noreply.github.com>
2026-09-11 13:27:36 +0530

    Add: find-anagram-mappings - submission-0

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        int[] result= new int[nums1.length];
+
+        for(int i=0;i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i] == nums2[j]){
+                    result[i] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
