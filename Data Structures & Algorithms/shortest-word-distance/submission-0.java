1d91137066f8d74a565a7dfb3a402e470916248c
sshah7 <45990517+sshah7@users.noreply.github.com>
2026-09-16 15:28:49 +0530

    Add: shortest-word-distance - submission-0

---
+++ Data Structures & Algorithms/shortest-word-distance/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int index1 = -1;
+        int index2 = -1;
+        int minDistance = wordsDict.length;
+        
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                index1 = i;
+            } else if (wordsDict[i].equals(word2)) {
+                index2 = i;
+            }
+            
+            if (index1 != -1 && index2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(index1 - index2));
+            }
+        }
+        
+        return minDistance;
+    }
+}
