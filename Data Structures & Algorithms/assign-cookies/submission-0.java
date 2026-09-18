cd7c58418878607f2802fb9e4277e7d1b981bd19
sshah7 <45990517+sshah7@users.noreply.github.com>
2026-09-18 10:36:51 +0530

    Add: assign-cookies - submission-0

---
+++ Data Structures & Algorithms/assign-cookies/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int child = 0;
+        for (int cookie = 0; child < g.length && cookie < s.length; cookie++) {
+            if (s[cookie] >= g[child]) {
+                child++;
+            }
+        }
+        return child;
+    }
+}
